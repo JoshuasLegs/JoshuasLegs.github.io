555f8cf5e9928edb9e1aa077bb41842ca616a586
francis <franciscotetremblay@gmail.com>
2022-03-12 12:42:59 -0500

    replaced modal gameplay video by inline videos

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import joshua from './assets/joshua.gif';
 import hang from './assets/hang.mp4';
 import rock from './assets/rock.png';
 import './App.scss';
-import { GameplayItem } from './components/gameplay-item/gameplay-item';
 import gameplay1png from "./assets/gameplay1.png";
 import gameplay1 from "./assets/gameplay1.mp4";
 import gameplay2png from "./assets/gameplay2.png";
 import gameplay2 from "./assets/gameplay2.mp4";
+import gameplay3png from "./assets/gameplay3.png";
+import gameplay3 from "./assets/gameplay3.mp4";
 import web from "./assets/web.mp4";
 import { useTranslation } from 'react-i18next';
+import { Fade } from "react-awesome-reveal";
+import { GameplayItem } from './components/gameplay-item/gameplay-item';
 
 window.addEventListener("load", () => {
   const body = document.querySelector("body");
@@ -19,7 +22,7 @@ window.addEventListener("load", () => {
 function App() {
 
   const { t, i18n } = useTranslation();
-  const paths = [[gameplay1png, gameplay1], [gameplay2png, gameplay2]]
+  const paths = [[gameplay3png, gameplay3], [gameplay2png, gameplay2], [gameplay1png, gameplay1]]
 
   const onLangChange = useCallback((l: string) => {
     i18n.changeLanguage(l);
@@ -30,26 +33,35 @@ function App() {
     <div className="App h-full" >
       <div className="section flex-col flex section2">
         <div className="flex flex-row justify-center items-center mt-5">
-          <span className='text-base md:text-3xl red pixelatus' >{t("A GAME BY:")}</span>
-          <div><a className='text-base md:text-3xl white ml-5 pixelatus' href="https://twitter.com/trespetitlegume">@trespetitlegume</a>
+          <span className='text-base md:text-3xl red-light pixelatus' >{t("A GAME BY:")}</span>
+          <div>
+            <a className='text-base md:text-3xl white ml-5 pixelatus' href="https://twitter.com/trespetitlegume">@trespetitlegume</a>
           </div>
         </div>
-        <div className='m-5 flex justify-center pixelatus text-lg white'><p className="text-width text-center uppercase">{t("styleDescription")}</p></div>
-        <div className='mt-24 flex flex-row content-center justify-center'>
-          {paths.map((p, i) => <GameplayItem key={i} img={p[0]} video={p[1]}></GameplayItem>)}
+        <Fade delay={100} duration={1500} className='m-5 flex justify-center pixelatus text-2xl md:text-4xl white'>
+          <p className="text-width text-center uppercase">{t("styleDescription")}</p>
+        </Fade>
+        <div className='mt-24 flex flex-col content-center items-center'>
+          {paths.map((p, i) => <GameplayItem className='text-width mb-10' img={p[0]} video={p[1]}></GameplayItem>)}
         </div>
-        <div className='mt-5 text-center pixelatus text-2xl md:text-4xl red m-5 mt-12 flex justify-center text-center'> <span className='text-width uppercase'>{t("gameplayTitle")}</span></div>
+        <Fade delay={100} duration={1500} className='text-center pixelatus text-2xl md:text-4xl red-light m-5 mt-0 flex justify-center text-center'>
+          <span className='text-width uppercase'>{t("gameplayTitle")}</span>
+        </Fade>
         <div className="flex flex-row items-center justify-center">
           <div className='half'>
             <video className='web w-full' muted autoPlay width={720} height={1280} loop playsInline controls={false} preload='auto'>
               <source src={web} type="video/mp4" />
             </video>
           </div>
-          <div className='text-left flex pixelatus text-2xl md:text-4xl red mr-10 half z-10'><span className='gif-text uppercase'>{t("webDescription")}</span></div>
+          <Fade delay={100} duration={1500} className='text-left flex pixelatus text-2xl md:text-4xl red-light mr-10 half z-10'>
+            <span className='gif-text uppercase'>{t("webDescription")}</span>
+          </Fade>
         </div>
 
         <div className="flex flex-row items-center justify-center">
-          <div className='text-right flex pixelatus text-2xl md:text-4xl red ml-10 z-10 half justify-end'><span className='gif-text uppercase'>{t("gripDescription")}</span></div>
+          <Fade delay={100} duration={1500} className='text-right flex pixelatus text-2xl md:text-4xl red-light ml-10 z-10 half justify-end'>
+            <span className='gif-text uppercase'>{t("gripDescription")}</span>
+          </Fade>
           <div className='half'>
             <video className='hang w-full' muted autoPlay width={720} height={1280} loop playsInline controls={false} preload='auto'>
               <source src={hang} type="video/mp4" />
@@ -57,8 +69,12 @@ function App() {
           </div>
         </div>
 
-        <div className='mt-5 text-center pixelatus text-2xl md:text-4xl white m-5 mt-24 flex justify-center'> <span className='text-width uppercase'>{t("gameDescription2")}</span></div>
-        <div className='mt-5 text-center pixelatus text-2xl md:text-4xl red m-5 flex justify-center'> <span className='text-width uppercase'>{t("gameDescription")}</span></div>
+        <Fade delay={200} duration={1500} className='mt-5 text-center pixelatus text-2xl md:text-4xl red-light m-5 mt-24 flex justify-center'>
+          <span className='text-width uppercase'>{t("gameDescription2")}</span>
+        </Fade>
+        <Fade delay={100} duration={1500} className='mt-5 text-center pixelatus text-2xl md:text-4xl white m-5 flex justify-center'>
+          <span className='text-width uppercase'>{t("gameDescription")}</span>
+        </Fade>
         <div className="flex flex-col w-full justify-center content-end">
           <div className='flex flex-row justify-center items-end'>
             <img src={rock} className="rock joshua-rock contain" alt="logo" />
@@ -89,7 +105,7 @@ function App() {
             <div className='white-square md:m-1'></div>
             <a className='text-xl md:text-2xl' href='/'>OST</a>
           </div>
-          <div className='flex flex-row justify-center text-2xl red pixelatus mb-4'>
+          <div className='flex flex-row justify-center text-2xl red-light pixelatus mb-4'>
             <button onClick={() => onLangChange("en")}>EN</button>
             <span className='md:text-3xl ml-1 mr-1'>/</span>
             <button onClick={() => onLangChange("fr")}>FR</button>
--- src/components/gameplay-item/gameplay-item.tsx
+++ src/components/gameplay-item/gameplay-item.tsx
@@ -1,43 +1,15 @@
 import "./gameplay-item.scss";
-import Modal from 'react-modal';
-import { useState } from "react";
-Modal.setAppElement('#root');
 
 interface IProps {
-    img: any,
-    video: any
+    img: string,
+    video: string
+    className?: string
 }
-export function GameplayItem({ img, video }: IProps) {
+export function GameplayItem({ img, video, className }: IProps) {
 
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const customStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-40%',
-            transform: 'translate(-50%, -50%)',
-            padding: "0",
-            maxWidth: 720
-        },
-    };
     return (
-        <div>
-            <img src={img} onClick={() => setIsOpen(x => !x)} className="gameplay-item-container m-2" alt="logo" />
-            <Modal
-                shouldCloseOnOverlayClick={true}
-                onRequestClose={() => setIsOpen(false)}
-                isOpen={modalIsOpen}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-                <video autoPlay muted controls={false} playsInline width="1280" height="720" loop>
-                    <source src={video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-
-            </Modal>
-        </div >
+        <video loop className={'gameplay ' + className} autoPlay muted poster={img} width={1280} height={720} controls preload="auto">
+            <source src={video} />
+        </video>
     )
 }
