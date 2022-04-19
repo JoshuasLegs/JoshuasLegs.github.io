8bcf859ef788cb608a9fcdc05117dcd8007a426c
francis <franciscotetremblay@gmail.com>
2022-04-19 12:49:07 -0400

    added loading screen. fixed section height

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { Home } from './components/home/home';
+import { LoadingScreen } from './components/loading-screen/loading-screen';
 import { Press } from './components/press/press';
 import { useOnLoad } from './hooks/onload';
 
@@ -8,16 +10,19 @@ import { useOnLoad } from './hooks/onload';
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+
   useOnLoad(() => {
     const body = document.querySelector("body");
     if (body) {
       body.scrollTo(0, document.body.scrollHeight);
-      //setTimeout(() => setLoading(false), 300);
+      setTimeout(() => setLoading(false), 300);
     }
   })
 
   return (
     <main>
+      {loading && <LoadingScreen></LoadingScreen>}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/press" element={<Press />} />
