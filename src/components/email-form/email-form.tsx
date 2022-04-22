ab304d85508faebf33cbfc8b6d646b8a8a1b3c56
francis <franciscotetremblay@gmail.com>
2022-04-22 09:32:14 -0400

    added gform for email list

--- src/components/email-form/email-form.tsx
+++ src/components/email-form/email-form.tsx
@@ -3,7 +3,7 @@ import "./email-form.scss";
 export function EmailForm() {
     return (
         <div className="email-form w-full">
-            <iframe className="w-full" src="https://docs.google.com/forms/d/e/1FAIpQLSe4UIKSuN4AsS-QAT3jp2HEBnlI08Sx1m0DzAa5OObLbkGnPw/viewform?embedded=true" height="500" marginHeight={0} marginWidth={0}>Loading…</iframe>
+            <iframe className="w-full" title="email-form" src="https://docs.google.com/forms/d/e/1FAIpQLSe4UIKSuN4AsS-QAT3jp2HEBnlI08Sx1m0DzAa5OObLbkGnPw/viewform?embedded=true" height="500" marginHeight={0} marginWidth={0}>Loading…</iframe>
         </div>
     )
 }
